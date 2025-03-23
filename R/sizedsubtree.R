3ec23c4d19a9f9702a9f6e010880c8d4ef9f65f4
Silvia Castiglione <silvia.castiglione@unina.it>
2025-03-23 08:50:02 +0000

    version 3.0.0

--- R/sizedsubtree.R
+++ R/sizedsubtree.R
@@ -20,13 +20,11 @@
 #'   Mondanaro, Marina Melchionna, Mirko Di Febbraro, Antonio Profico, Francesco
 #'   Carotenuto
 #' @examples
-#' \dontrun{
-#' data("DataOrnithodirans")
-#' DataOrnithodirans$treedino->treedino
-#'
-#' sizedsubtree(tree=treedino,Size=40)
+#'  \dontrun{
+#'  data("DataOrnithodirans")
+#'  DataOrnithodirans$treedino->treedino
+#'  sizedsubtree(tree=treedino,Size=40)->sst
 #' }
-
 
 sizedsubtree <-function(tree,Size=NULL,time.limit=10)
 {
